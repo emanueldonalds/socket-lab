14875a9dd38ecf9673f9e59ba4fcc0d8cdb0ccaa
Emanuel Donalds <emanueldonalds@edu.novia.fi>
2018-12-01 23:18:40 +0200

    Wrote program description

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
+//
+//This is a threaded server for updating stock market position data from clients
+//
+
 #include <stdio.h>
 
 int main()
 {
-    int i = 2;
-    int d;
-
-    d=3;
-
-    printf("i: %d", i);
+    
     return 0;
 }
